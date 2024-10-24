e7ba141fd0aa39025786345a5fa306cac618315b
Asikainen Timo <timo.asikainen@veripalvelu.fi>
2024-10-24 12:21:46 +0300

    adding a script to extract samples from donationdata (donor, donation, deferral and contact): in each data set, each column is sampled independently to scramble the data

---
+++ src/extract-donationdata-samples.R
@@ -0,0 +1,22 @@
+# extracts random samples from each data set in donation data
+# Each column is sampled independently of each other to break the data
+
+file <- param$data.file # defined in the Rmd file that should be done first
+load(file = file) # replaces the modified donationdata with the original one
+
+n = 100 # sample size
+for (m in names(donationdata)) {
+  data = donationdata[[m]]
+  output = NULL
+  for (c in colnames(data)) {
+    smpl = sample(data[[c]],n)
+    if (is.null(output))
+      output = data.frame(smpl)
+    else
+      output = cbind(output,data.frame(smpl))
+  }
+  colnames(output) = colnames(data)
+  write.table(output,file = paste(m,'.txt',sep=''),sep='\t',col.names = FALSE,row.names = FALSE,quote = FALSE)
+}
+
+
