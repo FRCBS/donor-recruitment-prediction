abdc3c8e14ebd2d32db55c99352db098ea224173
Asikainen Timo <timo.asikainen@veripalvelu.fi>
2024-10-24 12:37:34 +0300

    now replacing releaseID's with more or less random data so that no informatino should be leaked (eg. that a releaseID has a referral)

--- src/extract-donationdata-samples.R
+++ src/extract-donationdata-samples.R
@@ -5,11 +5,20 @@ file <- param$data.file # defined in the Rmd file that should be done first
 load(file = file) # replaces the modified donationdata with the original one
 
 n = 100 # sample size
+
+library(xfun)
+
 for (m in names(donationdata)) {
   data = donationdata[[m]]
   output = NULL
   for (c in colnames(data)) {
     smpl = sample(data[[c]],n)
+    if (c == 'releaseID') {
+      # Replace the releaseID's with essential random text data (both a-z and 0-9 included)
+      salt = rnorm(1)
+      smpl = substr(as.vector(sapply(paste(smpl,as.character(salt)),md5)),1,14)
+    }
+    
     if (is.null(output))
       output = data.frame(smpl)
     else
@@ -19,4 +28,4 @@ for (m in names(donationdata)) {
   write.table(output,file = paste(m,'.txt',sep=''),sep='\t',col.names = FALSE,row.names = FALSE,quote = FALSE)
 }
 
-
+?md5
