faef70613f14a0e645bb48e634350658d828d3ba
Asikainen Timo <timo.asikainen@veripalvelu.fi>
2025-01-03 15:54:28 +0200

    added NA checks for the key columns adjusted the working directory processing in blood-donor-recruitment-prediction.Rmd similarly

--- src/read-and-combine-source-data.R
+++ src/read-and-combine-source-data.R
@@ -1,35 +1,40 @@
-# nb! edit this to point to the directory where the source data files are stored
-
 param=list()
 param$wd = getwd()
 
-
 # getwd() might behave differently depending on the environment where it is run (console vs. Rmd),
 # therefore checking if working directory is set to the src folder and moving up if yes.
-if (grepl('[/\\]src[/\\]?',param$wd)) {
-  param$wd = sub('[/\\]src([/\\]?)$','\\1',param$wd)
+if (grepl('[/\\]src(data)[/\\]?',param$wd)) {
+  param$wd = sub('[/\\]src(data)?([/\\]?)$','\\2',param$wd)
 }
 
 setwd(file.path(param$wd,'srcdata'))
+
+# nb! If using Rstudio, working directory should be set to the donor-recruitment-prediction folder at this point.
+# The source data files are assumed to be saved under 
+# If not, please set it manually to point to the directory where the source data files can be found.
+param$wd
 
 tm <- as.POSIXlt(Sys.time(),"UTC")
 timestamp = strftime(tm,"%Y-%m-%dT%H_%M_%S%z")
 
 param$sink.file = NULL
+
+# nb! 
+# Uncomment the following line to save the log to a timestamped file in the log directory
 # param$sink.file = file.path(param$wd,'log',paste0('read-and-combine-source-data_',timestamp,'.log'))
 
 if (!is.null(param$sink.file)) {
   sink(param$sink.file)
 }
 
-# dir.create(file.path(param$wd,"results"),showWarnings = FALSE)
-# dir.create(file.path(param$wd,"log"),showWarnings = FALSE)
+dir.create(file.path(param$wd,"results"),showWarnings = FALSE)
+dir.create(file.path(param$wd,"log"),showWarnings = FALSE)
 # param$result.file = file.path(param$wd,"results","data.xlsx")
 
 # This is where is donationdata will be written
 datafile = file.path(param$wd,'donationdata.Rdata')
 
-# nb! adjust the header and sep (here tab, '\t') parameters as necessary
+# nb! adjust the header (here included) and sep (here tab, '\t') parameters as necessary
 t.donation=read.csv('donation_fake.csv',header=TRUE,colClasses=c(NA,NA,'Date',NA,NA),sep='\t')
 t.deferral=read.csv('deferral_fake.csv',header=TRUE,colClasses=c(NA,'POSIXct','POSIXct',NA),sep='\t')
 t.donor=read.csv('donor_fake.csv',header=TRUE,colClasses=c(NA,NA,NA,NA,'Date',NA),sep='\t')
@@ -75,17 +80,28 @@ for (o in others) {
   }
 }
 
+table = 'donation'
+check.na = c('releaseID','DonationDate')
+# col = 'DonationPlaceCode'
+for (col in check.na) {
+  nas = which(is.na(donationdata[[table]][[col]]))
+  if (length(nas) > 0) {
+    print(paste('WARNING:',length(nas),"NA's found in",table,'column',col,'these will be removed'))
+    donationdata[[table]] = donationdata[[table]][-nas,]
+  }
+}
+
 for (n in names(donationdata)) {
   print(paste('summary of',n))
   print(summary(donationdata[[n]]),na.rm=FALSE)
   print(colSums(is.na(donationdata[[n]])))
 }
 
-# The result has the semantics that elements are not repeated
-# setdiff(c(2,2,1),c(1))
-
 save(donationdata,file=datafile)
 
 if (!is.null(param$sink.file)) {
   sink()
 }
+
+
+str(donationdata)
