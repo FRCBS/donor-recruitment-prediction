78fd852fdb44cb16aaebf157386cff756d0e7ecf
Asikainen Timo <timo.asikainen@veripalvelu.fi>
2025-01-04 12:48:38 +0200

    donor-recruitment-prediction.Rmd: modified the parameters so that the file is by default read from whree read-and-combine-source-data.R writes it; in the latter file, added checks for data types and enumerations

--- src/read-and-combine-source-data.R
+++ src/read-and-combine-source-data.R
@@ -7,6 +7,8 @@ if (grepl('[/\\]src(data)[/\\]?',param$wd)) {
   param$wd = sub('[/\\]src(data)?([/\\]?)$','\\2',param$wd)
 }
 
+# Set the working directory to srcdata: this is assumed to exist before running the script and
+# the source files saved in the directory
 setwd(file.path(param$wd,'srcdata'))
 
 # nb! If using Rstudio, working directory should be set to the donor-recruitment-prediction folder at this point.
@@ -17,11 +19,12 @@ param$wd
 tm <- as.POSIXlt(Sys.time(),"UTC")
 timestamp = strftime(tm,"%Y-%m-%dT%H_%M_%S%z")
 
-param$sink.file = NULL
+# Specify a file where the logs will be written
+param$sink.file = file.path(param$wd,'log',paste0('read-and-combine-source-data_',timestamp,'.log'))
 
-# nb! 
-# Uncomment the following line to save the log to a timestamped file in the log directory
-# param$sink.file = file.path(param$wd,'log',paste0('read-and-combine-source-data_',timestamp,'.log'))
+# nb! Uncomment and run the following line to skip saving the output to a log file
+# and to have it printed as the lines are run.
+# param$sink.file = NULL
 
 if (!is.null(param$sink.file)) {
   sink(param$sink.file)
@@ -29,31 +32,58 @@ if (!is.null(param$sink.file)) {
 
 dir.create(file.path(param$wd,"results"),showWarnings = FALSE)
 dir.create(file.path(param$wd,"log"),showWarnings = FALSE)
-# param$result.file = file.path(param$wd,"results","data.xlsx")
 
 # This is where is donationdata will be written
 datafile = file.path(param$wd,'donationdata.Rdata')
 
+####
+# Reading source data files
 # nb! adjust the header (here included) and sep (here tab, '\t') parameters as necessary
-t.donation=read.csv('donation_fake.csv',header=TRUE,colClasses=c(NA,NA,'Date',NA,NA),sep='\t')
-t.deferral=read.csv('deferral_fake.csv',header=TRUE,colClasses=c(NA,'POSIXct','POSIXct',NA),sep='\t')
-t.donor=read.csv('donor_fake.csv',header=TRUE,colClasses=c(NA,NA,NA,NA,'Date',NA),sep='\t')
-t.contact=read.csv('contact_fake.csv',header=TRUE,colClasses=c(NA,NA,NA,'POSIXct',NA),sep='\t')
+t.donation=read.csv('donation.csv',header=TRUE,colClasses=c(NA,NA,'Date',NA,NA),sep='\t')
+t.deferral=read.csv('deferral.csv',header=TRUE,colClasses=c(NA,'POSIXct','POSIXct',NA),sep='\t')
+t.donor=read.csv('donor.csv',header=TRUE,colClasses=c(NA,NA,NA,NA,'Date',NA),sep='\t')
+t.contact=read.csv('contact.csv',header=TRUE,colClasses=c(NA,NA,NA,'POSIXct',NA),sep='\t')
 
 # nb! These lines are necessary to run only if the source data files did not include column names
 colnames(t.donation)=c("releaseID","BloodDonationTypeKey","DonationDate","DonationPlaceType","DonationPlaceCode")
 colnames(t.deferral)=c("releaseID","DeferralStartDate","DeferralEndDate","DonorAdverseReactionType")
 colnames(t.donor)=c("releaseID","Sex","PostalCode","PermissionToInvite","DateOfBirth","BloodGroup")
 colnames(t.contact)=c("releaseID","ContactChannel","ContactType","DateSent","DonationSiteCode")
+####
 
 # combine the (up to) four data frames read above into a single list called donationdata
 # nb! The correct functioning of the processing in blood-donor-recruitment-predction.Rmd requires that
 # an object with this name is loaded from param$data.file.
 donationdata = list(donation=t.donation,deferral=t.deferral,donor=t.donor,contact=t.contact)
 
-print('dimensions of source data frames')
+if (is.null(donationdata[['donation']])) 
+    print('ERROR: donation data missing in donationdata')
+
+if (is.null(donationdata[['donation']])) 
+  print('ERROR: donor data missing in donationdata')
+
+types = list()
+types[['donation']] = c("character","character","Date","character","character")
+types[['donor']] = c("character","character","character","character","Date","character")
+
+# print('dimensions of source data frames')
 for (n in names(donationdata)) {
-  print(paste(n,dim(donationdata[[n]])))
+  print(paste('dimensions of',n,paste(dim(donationdata[[n]]),collapse=',')))
+  data = donationdata[[n]]
+  
+  # convert releaseID to char
+  donationdata[[n]]$releaseID = as.character(data$releaseID)
+  
+  if (n %in% names(types)) {
+    classes = sapply(data, class)
+    for (i in 1:length(types[[n]])) {
+      spec = types[[n]][i]
+      actual = classes[i]
+      if (spec != 'character' && spec != actual) {
+        print(paste('ERROR: type of data in table',n,'is',actual,'while expecting',spec))
+      }
+    }
+  }
 }
 
 # data frames others than the donor data frame
@@ -82,7 +112,6 @@ for (o in others) {
 
 table = 'donation'
 check.na = c('releaseID','DonationDate')
-# col = 'DonationPlaceCode'
 for (col in check.na) {
   nas = which(is.na(donationdata[[table]][[col]]))
   if (length(nas) > 0) {
@@ -91,17 +120,38 @@ for (col in check.na) {
   }
 }
 
-for (n in names(donationdata)) {
-  print(paste('summary of',n))
-  print(summary(donationdata[[n]]),na.rm=FALSE)
-  print(colSums(is.na(donationdata[[n]])))
+# Check the essential enumerations here
+# Other than enumerated values are just listed but not removed
+# Special conventions given below under each enumeration
+enumerations = list()
+enumerations[['donor,BloodGroup']] = 'A+,A-,AB+,AB-,B+,B-,O+,O-'
+enumerations[['donor,Sex']] = 'Male,Female'
+enumerations[['donation,BloodDonationTypeKey']] = 'Whole Blood (K)'
+# Only donations/rows with this values (Whole Blood (K)) are included in the analysis
+enumerations[['donation,DonationPlaceType']] = 'Office'
+# Values other than Office are classified as Mobile
+
+for (e in names(enumerations)) {
+  evals = strsplit(enumerations[[e]],',')[[1]]
+  tabcol = strsplit(e,',')[[1]]
+  tab = tabcol[1]
+  col = tabcol[2]
+  data = donationdata[[tab]][[col]]
+  xval = which(!data %in% evals)
+  n = length(xval)
+  if (n > 0) {
+    print(paste(n,'values in table',tab,'column',col,'that are not in the expected values:',paste(evals,collapse=',')))
+    print(table(data[xval],useNA='ifany'))
+  }
 }
 
+# Save the data to the datafile (by default at the root level of the local repository)
 save(donationdata,file=datafile)
 
 if (!is.null(param$sink.file)) {
   sink()
+  
+  # Output the log to the console for convenience
+  fileName <- param$sink.file
+  cat(readChar(fileName, file.info(fileName)$size))
 }
-
-
-str(donationdata)
